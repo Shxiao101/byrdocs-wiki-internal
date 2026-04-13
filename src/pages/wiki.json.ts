aac9f12e8a9d5f802495ff508d2a8148e5e39c71
cppHusky <cpphusky@outlook.com>
2026-04-13 14:03:10 +0800

    added wiki.json

---
+++ src/pages/wiki.json.ts
@@ -0,0 +1,40 @@
+import type {APIRoute} from "astro";
+import {getCollection} from "astro:content";
+export const GET:APIRoute=async ()=>{
+	const parseMetadata=(exam=>{
+		const type="test";
+		const id=exam.data.来源??undefined;
+		const url=`https://wiki.byrdocs.org/${exam.id}`;
+		const college=exam.data.学院??undefined;
+		const course={
+			type:exam.data.类型,
+			name:exam.data.科目,
+		};
+		const filetype="wiki";
+		const time={
+			start:exam.data.时间.substr(0,4),
+			end:exam.data.时间.substr(5,4),
+			semester:exam.data.时间.substr(12,1)==="一"?"First":"Second",
+			stage:exam.data.阶段,
+		};
+		const content=[
+			"原题",
+			...((exam.data.答案完成度==="完整"||exam.data.答案完成度==="完鷘可靠")?["答案"]:[]),
+		];
+		return {
+			type,
+			id,
+			data:{
+				college,
+				course,
+				filetype,
+				time,
+				content,
+			},
+			url,
+		};
+	});
+	const exams=await getCollection("exams");
+	const result=exams.map(exam=>parseMetadata(exam));
+	return Response.json(result);
+};
