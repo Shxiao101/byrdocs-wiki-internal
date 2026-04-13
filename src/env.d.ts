bd5c55b784d5c183184cc4bb9e72392c9af3addd
cppHusky <cpphusky@outlook.com>
2026-04-13 21:57:30 +0800

    remove theme icon toggle; add show/hide answers toggle

---
+++ src/env.d.ts
@@ -0,0 +1,32 @@
+/// <reference types="astro/client" />
+
+type ExamChoiceRevealMode="answers"|"checked";
+
+interface ExamChoiceState{
+	selected:string[];
+	revealed:boolean;
+	mode?:ExamChoiceRevealMode;
+}
+
+interface ExamStateData{
+	blanks:Record<string,true>;
+	solutions:Record<string,true>;
+	choices:Record<string,ExamChoiceState>;
+}
+
+interface ExamStateController{
+	read:()=>ExamStateData;
+	update:(updater:(state:ExamStateData)=>ExamStateData)=>ExamStateData;
+	sync:()=>void;
+	toggleAll:()=>void;
+	getAllShown:()=>boolean;
+	hasRevealable:()=>boolean;
+	events:{
+		change:string;
+		setAll:string;
+	};
+}
+
+interface Window{
+	__examState?:ExamStateController;
+}
